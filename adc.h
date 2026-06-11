a5a6625768d6d6d55b8191b94b562ecc9f1a7304
GaneshSai-emb <ganeshsai.emb@gmail.com>
2026-06-11 21:57:48 +0530

    Update adc.h

--- adc.h
+++ adc.h
@@ -1,3 +1,7 @@
+
+/* Initialize ADC module */
 void Init_ADC(void);
-void Read_ADC(u8 chno, f32 *eAR, u32 * AdvDVal);
-int  read_temp(void);
+/* Read ADC value and convert into voltage */
+void Read_ADC(u8 chno, f32 *eAR, u32 *AdvDVal);
+/* Read temperature value from ADC sensor */
+int read_temp(void);
